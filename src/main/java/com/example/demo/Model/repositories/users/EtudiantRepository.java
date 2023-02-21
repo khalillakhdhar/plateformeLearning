6f5372bb8f2f6cfcae08b16eda27e1fd2fee6e5e
khalil <khalillakhdharatc@gmail.com>
2023-02-21 16:57:31 +0100

    Update EtudiantRepository.java

--- src/main/java/com/example/demo/Model/repositories/users/EtudiantRepository.java
+++ src/main/java/com/example/demo/Model/repositories/users/EtudiantRepository.java
@@ -1,9 +1,12 @@
 package com.example.demo.Model.repositories.users;
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import com.example.demo.Model.entity.education.Cours;
 import com.example.demo.Model.entity.users.Etudiant;
 
 public interface EtudiantRepository extends PagingAndSortingRepository<Etudiant, Long> {
-
+List<Etudiant> findByCours(List<Cours> cours);
 }
